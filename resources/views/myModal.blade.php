a43d4aa92e5a6f345e26036c5e53bd236241dfb1
Carlinhos01 <carloseduardoce47437@gmail.com>
2024-05-20 19:29:12 -0300

    primeiro commit

---
+++ resources/views/myModal.blade.php
@@ -0,0 +1,110 @@
+<div id="myModal" class="modal-cont-user">
+    <div class="modal-content-user">
+        <!-- Barra de Navegação -->
+        <div class="desc-user-inform">
+
+        </div>
+
+        <div class="navbar">
+            <a href="#" class="nav-item" id="btn-dados"
+                onclick="toggleModalActive('dados'); toggleNavActive('btn-dados')">Dados</a>
+            <a href="#" class="nav-item" id="btn-carteirinha"
+                onclick="toggleModalActive('carteirinha'); toggleNavActive('btn-carteirinha')">Carteirinha</a>
+            <a href="#" class="nav-item" id="btn-chat"
+                onclick="toggleModalActive('chat'); toggleNavActive('btn-chat')">Chat</a>
+            <a href="#" class="nav-item" id="btn-responsavel"
+                onclick="toggleModalActive('responsavel'); toggleNavActive('btn-responsavel')">Responsável</a>
+            <div class="line"></div>
+        </div>
+        <div class="modal-screen active cont-modal-inicio" id="inicio">
+
+        </div>
+        <div class="modal-screen cont-modal-user-dados" id="dados">
+            <div class="cont-dados">
+                <div class="cont-0">
+                    <h3>Id:</h3>
+                    <div class="cont-nome">
+                        <p>23047</p>
+                    </div>
+                </div>
+                <div class="cont-1">
+                    <h3>nome:</h3>
+                    <div class="cont-nome">
+                        <p>Jefferson Lopes de Sousa</p>
+                    </div>
+                </div>
+                <div class="cont-2">
+                    <h3>Email:</h3>
+                    <div class="cont-email">
+                        <p>jeffersonlopes1201@gmail.com</p>
+                    </div>
+                </div>
+                <div class="cont-3">
+                    <h3>Data de Nascimento:</h3>
+                    <div class="cont-dtNasc">
+                        <p>12/01/2007</p>
+                    </div>
+                </div>
+                <div class="cont-4">
+                    <h3>Genero:</h3>
+                    <div class="cont-genero">
+                        <p>Masculino</p>
+                    </div>
+                </div>
+                <div class="cont-4">
+                    <h3>Telefone:</h3>
+                    <div class="cont-genero">
+                        <p>(11)98451-6598</p>
+                    </div>
+                </div>
+                <div class="cont-5">
+                    <h3>curço/Série/Ensino:</h3>
+                    <div class="cont-Cse">
+                        <p>Técnico em Desenvolvimento de Sistemas</p>
+                    </div>
+                </div>
+                <div class="cont-6">
+                    <h3>Instituição:</h3>
+                    <div class="cont-nome">
+                        <p>Escola Técnica Estadual de Guarulhos</p>
+                    </div>
+                </div>
+                <div class="cont-7">
+                    <h3>nome:</h3>
+                    <div class="cont-nome">
+                        <p>Jefferson Lopes de Sousa</p>
+                    </div>
+                </div>
+                <div class="cont-8">
+                    <h3>Pessoa com Deficiencia:</h3>
+                    <div class="cont-nome">
+                        <p>não</p>
+                    </div>
+                </div>
+                <div class="cont-8">
+                    <h3>RG:</h3>
+                    <div class="cont-nome">
+                        <p>63.473.357-7</p>
+                    </div>
+                </div>
+                <div class="cont-8">
+                    <h3>CPF:</h3>
+                    <div class="cont-nome">
+                        <p>588.546.349.35</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal-screen cont-modal-user-carteirinha" id="carteirinha">
+
+        </div>
+        <!-- chat -->
+        <div class="modal-screen cont-modal-user-chat" id="chat">
+            
+        </div>
+        <!-- fim chat -->
+        <div class="modal-screen cont-modal-user-responsavel" id="responsavel">
+
+        </div>
+    </div>
+</div>
